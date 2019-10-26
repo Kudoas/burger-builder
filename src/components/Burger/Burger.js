0a2d3261f40ef665a8b9424cfad26b63a117de0a
Daichi Kudo <sakanactor624@gmail.com>
2019-10-26 22:21:34 +0900

    :lipstick: add burger ingredients state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import classes from './burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    const transformdeIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformdeIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
