db6e96bacaf5c3fc25e5541c0c7db21d38cd60b2
Daichi Kudo <sakanactor624@gmail.com>
2019-10-27 12:21:16 +0900

    :boom: add empty state message

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const transformdeIngredients = Object.keys(props.ingredients)
+    let transformdeIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformdeIngredients.length === 0) {
+        transformdeIngredients = <p>Please start adding ingredients!</p>
+    }
+    console.log(transformdeIngredients)
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
