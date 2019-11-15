f3e05262c84ba3a2815b49ce1d0d2726b85aa0a5
Daichi Kudo <sakanactor624@gmail.com>
2019-11-15 21:22:17 +0900

    :sparkles: burger-UI is almost complete.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import classes from './burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    let transformdeIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
@@ -13,14 +13,18 @@ const burger = (props) => {
         .reduce((arr, el) => {
             return arr.concat(el)
         }, []);
-    if (transformdeIngredients.length === 0) {
-        transformdeIngredients = <p>Please start adding ingredients!</p>
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
     }
-    console.log(transformdeIngredients)
+
+    // debug
+    console.log('Object.keys', Object.keys(props.ingredients));
+    console.log('transform', transformedIngredients);
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            {transformdeIngredients}
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
